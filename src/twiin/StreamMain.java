1b9986a51fb7f16587515bc133353910a24b9d50
Eugene Borisov <eugeni.borisov@gmail.com>
2022-09-26 09:15:41 +0300

    adjustment StreamMain

--- src/twiin/StreamMain.java
+++ src/twiin/StreamMain.java
@@ -7,7 +7,7 @@ public class StreamMain {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
         intList.stream()
-                .filter(x -> x >0)
+                .filter(x -> x > 0)
                 .filter(x -> x % 2 == 0)
                 .sorted(Comparator.naturalOrder())
                 .forEach(System.out::println);
